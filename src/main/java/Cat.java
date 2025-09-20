791a8501cde25d4051c08f14847f32525ed7ed64
AWicls <awioop@outlook.com>
2025-09-20 19:02:44 +0800

    完善cat类

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,5 +1,57 @@
 public class Cat {
-    public static void main(String[] args) {
-        
+
+    private String name;
+    private int age;
+    private String id;
+    private String owner;
+
+    public Cat(String name, int age, String id, String owner) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.owner = owner;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id='" + id + '\'' +
+                ", owner='" + owner + '\'' +
+                '}';
+    }
+
 }
